4a41387fee7069c6d8a733673b355c7bc8272974
ZMT <ZMT819@126.com>
2021-10-15 16:08:17 +0800

    fix forward

--- src/main/forward/index.js
+++ src/main/forward/index.js
@@ -3,7 +3,7 @@
  * @Author: zmt
  * @Date: 2021-10-09 15:21:49
  * @LastEditors: zmt
- * @LastEditTime: 2021-10-15 08:35:05
+ * @LastEditTime: 2021-10-15 10:04:34
  */
 /**
  * 1.链接数据库
@@ -34,16 +34,22 @@ export default class ForwardsDatabase {
       // 链接目标数据库
       this.targetConnection = await connect({ type: this.targetForm.databaseType, form: this.targetForm }, true)
 
-      // await this.startForward()
+      await this.startForward()
     } catch (e) {
+      // 链接异常
       throw new Error(e)
     }
   }
 
   async startForward () {
-    this.sourceColumn = await this.sourceConnection.getColum(this.sourceForm.tableName)
+    try {
+      this.sourceColumn = await this.sourceConnection.getColum(this.sourceForm.tableName)
 
-    this.targetColumn = await this.targetConnection.getColum(this.targetForm.tableName)
+      this.targetColumn = await this.targetConnection.getColum(this.targetForm.tableName)
+    } catch (e) {
+      // 获取列表名异常处理
+      throw new Error('获取列表名称异常')
+    }
 
     if (this.sourceColumn.length === 0 || this.targetColumn.length === 0) return
 
@@ -90,7 +96,13 @@ export default class ForwardsDatabase {
         await this.handleUnInsertData()
       }
     } catch (e) {
-      throw new Error(`${this.sourceForm.connectString}-${this.sourceForm.tableName} select data error, reason: ${e}`)
+      // 查询异常处理
+      const obj = {
+        time: new Date().toLocaleTimeString(),
+        database: `${this.sourceForm.connectString}-${this.sourceForm.tableName}`,
+        position: { pageNum: this.pageNum, pageSize: this.pageSize }
+      }
+      fs.appendFileSync(`${config.savePath}/selectData-forward.log`, JSON.stringify(obj))
     }
   }
 
@@ -98,8 +110,9 @@ export default class ForwardsDatabase {
     try {
       await this.targetConnection.insertBatch(this.targetForm.tableName, this.targetColumn, cacheArr)
     } catch (e) {
+      // 入库异常处理
       this.unInsertData.push(cacheArr)
-      throw new Error(e)
+      // throw new Error(e)
     }
   }
 
@@ -110,8 +123,13 @@ export default class ForwardsDatabase {
       await this.targetConnection.close()
       this.unInsertData = []
     } catch (e) {
-      fs.writeFileSync(`${config.savePath}/unInsertData-forward.txt`, JSON.stringify(this.unInsertData))
-      throw new Error(e)
+      // 查询异常处理
+      const obj = {
+        time: new Date().toLocaleTimeString(),
+        database: `${this.targetForm.connectString}-${this.targetForm.tableName}`,
+        data: this.unInsertData
+      }
+      fs.appendFileSync(`${config.savePath}/unInsertData-forward.txt`, JSON.stringify(obj))
     }
   }
 }
